44578734e5c1b942d9163350e1194501dfbb8c55
javert <green19113@gmail.com>
2025-07-16 17:26:36 +0800

    更新 river_bank_sql_for_bulid_table_0716.sql

--- finalProj/SQL/river_bank_sql_for_bulid_table_0716.sql
+++ finalProj/SQL/river_bank_sql_for_bulid_table_0716.sql
@@ -265,6 +265,110 @@ CREATE TABLE point_transactions (
 );
 ------------------------------------------------------------------------------------javert結束
 
+
+
+------------------------------------------------------------------------------johnson開始
+/*
+DROP TABLE ticket_issue_cost_attachment;
+DROP TABLE ticket_to_administrator;
+DROP TABLE ticket_attachment;
+DROP TABLE ticket_comment;
+DROP TABLE issue_type_and_ticket;
+DROP TABLE ticket;
+DROP TABLE issue_type;
+DROP TABLE vendor;
+*/
+------------------------------------------------------------------------------
+create table ticket (
+    id INT PRIMARY KEY IDENTITY(1,1),				 -- ticket流水號
+    reporter_id INT NULL,							 -- 創建者ID（FK to Users.id）
+    title NVARCHAR(255) NOT NULL,					 -- 問題標題
+    assigner_id INT NULL ,							 -- 被指派者（可為 NULL）
+    [status] NVARCHAR(255) NOT NULL DEFAULT 'to do', -- 問題狀態
+    issue_description NVARCHAR(MAX) NOT NULL,		 -- 問題敘述
+    Cost INT ,										 -- 修繕費用
+    action_time DATETIME,							 -- 更改時間
+    action_by INT,									 -- 誰更動的（FK）
+    [start_date] DATETIME NOT NULL DEFAULT GETDATE(),-- 創建時間
+    end_date DATETIME,								 -- 結束時間
+    notes VARCHAR(255),								 -- 備註
+    community_id INT NOT NULL,						 -- 所屬社區（FK）
+    FOREIGN KEY (reporter_id) REFERENCES users(users_id)ON DELETE SET NULL,
+    FOREIGN KEY (assigner_id) REFERENCES users(users_id),
+    FOREIGN KEY (action_by) REFERENCES users(users_id),
+    FOREIGN KEY (community_id) REFERENCES community(id)
+);
+
+------------------------------------------------------------------------------
+
+create table issue_type(
+    id INT PRIMARY KEY IDENTITY(1,1),					--問題類別流水號
+    issue_type_name NVARCHAR(255)						--問題類別名稱
+)
+------------------------------------------------------------------------------
+
+create table issue_type_and_ticket(
+    id INT PRIMARY KEY IDENTITY(1,1),	
+    ticket_id	INT,									--ticket流水號
+    issue_type_id INT,									--問題類別流水號	
+    FOREIGN KEY (ticket_id) REFERENCES ticket(id),
+    FOREIGN KEY (issue_type_id) REFERENCES issue_type(id)
+)
+------------------------------------------------------------------------------
+create table ticket_comment(
+    id INT PRIMARY KEY IDENTITY(1,1),					--ticket_comment流水號
+    ticket_id INT,										--ticket流水號
+    comment_time DATETIME NOT NULL DEFAULT getDate(),	--留言時間
+    commenter_id INT ,									--留言ID
+    comment	VARCHAR(255),								--留言
+    FOREIGN KEY (ticket_id) REFERENCES ticket(id),
+    FOREIGN KEY (commenter_id) REFERENCES users(users_id)ON DELETE SET NULL
+)
+------------------------------------------------------------------------------
+
+create table ticket_attachment(
+    id	INT PRIMARY KEY	IDENTITY(1,1),					--ticket_attachment流水號
+    uploaded_by	INT NOT NULL,							--上傳者
+    ticket_id INT,										--ticket流水號
+    comment_ID	INT,									--留言ID
+    [file_name] NVARCHAR(255),							--檔案名稱	
+    [file] VARBINARY(MAX),								--檔案
+    image_url	VARBINARY(MAX),							--圖	
+    uploaded_at	DATETIME NOT NULL DEFAULT getDate(),	--上傳時間
+    FOREIGN KEY (uploaded_by) REFERENCES users(users_id),
+    FOREIGN KEY (ticket_id) REFERENCES ticket(id),
+    FOREIGN KEY (comment_ID) REFERENCES ticket_comment(id)
+)
+------------------------------------------------------------------------------
+create table vendor(
+    vendor_ID INT PRIMARY KEY IDENTITY(1,1),			--廠商 ID	
+    vendor_name	VARCHAR(255),							--廠商名稱	
+    contact_person VARCHAR(255),						--聯絡人姓名	
+    phone_number VARCHAR(255),							--聯絡電話	
+    [address] VARCHAR(255),								--地址		
+    notes VARCHAR(255),									--其他備註	
+    tax_ID_number	int									--廠商統編	
+);
+------------------------------------------------------------------------------
+create table ticket_to_administrator(
+    id	INT PRIMARY KEY IDENTITY(1,1),					--流水號	
+    ticket_id	INT,									--ticket流水號	
+    vendor_ID	INT,									--廠商id	
+    FOREIGN KEY (ticket_id) REFERENCES ticket(id),
+    FOREIGN KEY (vendor_ID) REFERENCES vendor(vendor_ID)
+)
+------------------------------------------------------------------------------
+create table ticket_issue_cost_attachment(
+    id	INT PRIMARY KEY IDENTITY(1,1),					--Cost_attachment流水號	
+    ticket_id INT NOT NULL,								--ticket流水號
+    cost INT NOT NULL,									--修繕費用
+    [file_name]	NVARCHAR(255) NOT NULL,					--檔案名稱	
+    [file]	VARBINARY(MAX) NOT NULL,					--檔案		
+    vendor_ID	INT,									--廠商id	
+    FOREIGN KEY (ticket_id) REFERENCES ticket(id),
+    FOREIGN KEY (vendor_ID) REFERENCES vendor(vendor_ID)
+)
+------------------------------------------------------------------------------johnson結束
 ------------------------------------------------------------------------------------yu開始
 /*
 DROP TABLE if exists faq_faq_keyword;
@@ -558,109 +662,6 @@ CREATE TABLE feedback_status_history (
 );
 
 ------------------------------------------------------------------------------yu結束
-
-------------------------------------------------------------------------------johnson開始
-/*
-DROP TABLE ticket_issue_cost_attachment;
-DROP TABLE ticket_to_administrator;
-DROP TABLE ticket_attachment;
-DROP TABLE ticket_comment;
-DROP TABLE issue_type_and_ticket;
-DROP TABLE ticket;
-DROP TABLE issue_type;
-DROP TABLE vendor;
-*/
-------------------------------------------------------------------------------
-create table ticket (
-    id INT PRIMARY KEY IDENTITY(1,1),				 -- ticket流水號
-    reporter_id INT NULL,							 -- 創建者ID（FK to Users.id）
-    title NVARCHAR(255) NOT NULL,					 -- 問題標題
-    assigner_id INT NULL ,							 -- 被指派者（可為 NULL）
-    [status] NVARCHAR(255) NOT NULL DEFAULT 'to do', -- 問題狀態
-    issue_description NVARCHAR(MAX) NOT NULL,		 -- 問題敘述
-    Cost INT ,										 -- 修繕費用
-    action_time DATETIME,							 -- 更改時間
-    action_by INT,									 -- 誰更動的（FK）
-    [start_date] DATETIME NOT NULL DEFAULT GETDATE(),-- 創建時間
-    end_date DATETIME,								 -- 結束時間
-    notes VARCHAR(255),								 -- 備註
-    community_id INT NOT NULL,						 -- 所屬社區（FK）
-    FOREIGN KEY (reporter_id) REFERENCES users(users_id)ON DELETE SET NULL,
-    FOREIGN KEY (assigner_id) REFERENCES users(users_id),
-    FOREIGN KEY (action_by) REFERENCES users(users_id),
-    FOREIGN KEY (community_id) REFERENCES community(id)
-);
-
-------------------------------------------------------------------------------
-
-create table issue_type(
-    id INT PRIMARY KEY IDENTITY(1,1),					--問題類別流水號
-    issue_type_name NVARCHAR(255)						--問題類別名稱
-)
-------------------------------------------------------------------------------
-
-create table issue_type_and_ticket(
-    id INT PRIMARY KEY IDENTITY(1,1),	
-    ticket_id	INT,									--ticket流水號
-    issue_type_id INT,									--問題類別流水號	
-    FOREIGN KEY (ticket_id) REFERENCES ticket(id),
-    FOREIGN KEY (issue_type_id) REFERENCES issue_type(id)
-)
-------------------------------------------------------------------------------
-create table ticket_comment(
-    id INT PRIMARY KEY IDENTITY(1,1),					--ticket_comment流水號
-    ticket_id INT,										--ticket流水號
-    comment_time DATETIME NOT NULL DEFAULT getDate(),	--留言時間
-    commenter_id INT ,									--留言ID
-    comment	VARCHAR(255),								--留言
-    FOREIGN KEY (ticket_id) REFERENCES ticket(id),
-    FOREIGN KEY (commenter_id) REFERENCES users(users_id)ON DELETE SET NULL
-)
-------------------------------------------------------------------------------
-
-create table ticket_attachment(
-    id	INT PRIMARY KEY	IDENTITY(1,1),					--ticket_attachment流水號
-    uploaded_by	INT NOT NULL,							--上傳者
-    ticket_id INT,										--ticket流水號
-    comment_ID	INT,									--留言ID
-    [file_name] NVARCHAR(255),							--檔案名稱	
-    [file] VARBINARY(MAX),								--檔案
-    image_url	VARBINARY(MAX),							--圖	
-    uploaded_at	DATETIME NOT NULL DEFAULT getDate(),	--上傳時間
-    FOREIGN KEY (uploaded_by) REFERENCES users(users_id),
-    FOREIGN KEY (ticket_id) REFERENCES ticket(id),
-    FOREIGN KEY (comment_ID) REFERENCES ticket_comment(id)
-)
-------------------------------------------------------------------------------
-create table vendor(
-    vendor_ID INT PRIMARY KEY IDENTITY(1,1),			--廠商 ID	
-    vendor_name	VARCHAR(255),							--廠商名稱	
-    contact_person VARCHAR(255),						--聯絡人姓名	
-    phone_number VARCHAR(255),							--聯絡電話	
-    [address] VARCHAR(255),								--地址		
-    notes VARCHAR(255),									--其他備註	
-    tax_ID_number	int									--廠商統編	
-);
-------------------------------------------------------------------------------
-create table ticket_to_administrator(
-    id	INT PRIMARY KEY IDENTITY(1,1),					--流水號	
-    ticket_id	INT,									--ticket流水號	
-    vendor_ID	INT,									--廠商id	
-    FOREIGN KEY (ticket_id) REFERENCES ticket(id),
-    FOREIGN KEY (vendor_ID) REFERENCES vendor(vendor_ID)
-)
-------------------------------------------------------------------------------
-create table ticket_issue_cost_attachment(
-    id	INT PRIMARY KEY IDENTITY(1,1),					--Cost_attachment流水號	
-    ticket_id INT NOT NULL,								--ticket流水號
-    cost INT NOT NULL,									--修繕費用
-    [file_name]	NVARCHAR(255) NOT NULL,					--檔案名稱	
-    [file]	VARBINARY(MAX) NOT NULL,					--檔案		
-    vendor_ID	INT,									--廠商id	
-    FOREIGN KEY (ticket_id) REFERENCES ticket(id),
-    FOREIGN KEY (vendor_ID) REFERENCES vendor(vendor_ID)
-)
-------------------------------------------------------------------------------johnson結束
 ------------------------------------------------------------------------------julie開始
 
 CREATE TABLE parking_type (
