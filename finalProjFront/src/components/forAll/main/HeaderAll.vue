8465177cc39f0c0e5b7dd84f5f2d2588c94caf93
javert <green19113@gmail.com>
2025-07-18 14:23:45 +0800

    merge tim modify login fail sweetalert

--- finalProjFront/src/components/forAll/main/HeaderAll.vue
+++ finalProjFront/src/components/forAll/main/HeaderAll.vue
@@ -872,9 +872,12 @@ body {
     padding: 1px 4px;
   }
 }
+
 .admin-button {
-  background-color: #343a40; /* 深灰色背景 */
-  color: #fff; /* 白字 */
+  background-color: #343a40;
+  /* 深灰色背景 */
+  color: #fff;
+  /* 白字 */
   border: none;
   padding: 10px 20px;
   border-radius: 8px;
@@ -886,7 +889,8 @@ body {
 }
 
 .admin-button:hover {
-  background-color: #495057; /* hover 淺一點 */
+  background-color: #495057;
+  /* hover 淺一點 */
   transform: translateY(-2px);
 }
 
@@ -894,5 +898,4 @@ body {
   background-color: #212529;
   transform: scale(0.98);
 }
-
 </style>
